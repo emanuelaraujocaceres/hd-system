062f5f7cb4a3c0f8638876d3503e5647ff336961
Emanuel Araujo Caceres <emanuearaujocaceres@gmail.com>
2026-07-30 14:04:59 -0300

    fix(SQL): use ::text cast in COUNT queries to match CASE expression type

--- supabase-corrigir-dados-historicos.sql
+++ supabase-corrigir-dados-historicos.sql
@@ -23,7 +23,7 @@ WHERE store_branch_id::text IN ('br-01', 'br-02', 'br-03');
 
 SELECT 'sales' AS tabela, COUNT(*) AS linhas_com_uuid_correto
 FROM sales
-WHERE store_branch_id IN ('f3265a77-5946-5cd3-b09c-725ac4d26952'::uuid, '32b31da2-00e8-570c-8a10-2d6ae23c9eee'::uuid, '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc'::uuid);
+WHERE store_branch_id::text IN ('f3265a77-5946-5cd3-b09c-725ac4d26952', '32b31da2-00e8-570c-8a10-2d6ae23c9eee', '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc');
 
 -- =============================================================
 -- 3. CORRIGIR store_branch_id em cash_sessions
@@ -39,7 +39,7 @@ WHERE store_branch_id::text IN ('br-01', 'br-02', 'br-03');
 
 SELECT 'cash_sessions' AS tabela, COUNT(*) AS linhas_com_uuid_correto
 FROM cash_sessions
-WHERE store_branch_id IN ('f3265a77-5946-5cd3-b09c-725ac4d26952'::uuid, '32b31da2-00e8-570c-8a10-2d6ae23c9eee'::uuid, '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc'::uuid);
+WHERE store_branch_id::text IN ('f3265a77-5946-5cd3-b09c-725ac4d26952', '32b31da2-00e8-570c-8a10-2d6ae23c9eee', '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc');
 
 -- =============================================================
 -- 4. CORRIGIR store_branch_id em stock_movements
@@ -55,7 +55,7 @@ WHERE store_branch_id::text IN ('br-01', 'br-02', 'br-03');
 
 SELECT 'stock_movements' AS tabela, COUNT(*) AS linhas_com_uuid_correto
 FROM stock_movements
-WHERE store_branch_id IN ('f3265a77-5946-5cd3-b09c-725ac4d26952'::uuid, '32b31da2-00e8-570c-8a10-2d6ae23c9eee'::uuid, '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc'::uuid);
+WHERE store_branch_id::text IN ('f3265a77-5946-5cd3-b09c-725ac4d26952', '32b31da2-00e8-570c-8a10-2d6ae23c9eee', '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc');
 
 -- =============================================================
 -- 5. CORRIGIR store_branch_id em financial_transactions
@@ -71,7 +71,7 @@ WHERE store_branch_id::text IN ('br-01', 'br-02', 'br-03');
 
 SELECT 'financial_transactions' AS tabela, COUNT(*) AS linhas_com_uuid_correto
 FROM financial_transactions
-WHERE store_branch_id IN ('f3265a77-5946-5cd3-b09c-725ac4d26952'::uuid, '32b31da2-00e8-570c-8a10-2d6ae23c9eee'::uuid, '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc'::uuid);
+WHERE store_branch_id::text IN ('f3265a77-5946-5cd3-b09c-725ac4d26952', '32b31da2-00e8-570c-8a10-2d6ae23c9eee', '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc');
 
 -- =============================================================
 -- 6. CORRIGIR store_branch_id em products
@@ -87,7 +87,7 @@ WHERE store_branch_id::text IN ('br-01', 'br-02', 'br-03');
 
 SELECT 'products' AS tabela, COUNT(*) AS linhas_com_uuid_correto
 FROM products
-WHERE store_branch_id IN ('f3265a77-5946-5cd3-b09c-725ac4d26952'::uuid, '32b31da2-00e8-570c-8a10-2d6ae23c9eee'::uuid, '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc'::uuid);
+WHERE store_branch_id::text IN ('f3265a77-5946-5cd3-b09c-725ac4d26952', '32b31da2-00e8-570c-8a10-2d6ae23c9eee', '62a2b07b-4237-559a-a2cd-e0fc7dcbecdc');
 
 -- =============================================================
 -- 7. VERIFICAR E CORRIGIR PREÇO DO HEINEKEN 269ml
